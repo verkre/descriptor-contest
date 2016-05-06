20f6930882fa51d8b32eeb8a419fa29d2cfecbcd
Martin Häcker <spamfaenger@gmx.de>
2016-05-06 13:40:10 +0200

    Comments

--- schema.sql
+++ schema.sql
@@ -7,19 +7,20 @@ create table contests (
 drop table if exists descriptors;
 create table descriptors (
     id integer primary key autoincrement,
-    value text not null,
+    value text not null, -- REFACT rename to 'name' for consistency?
     contest_id integer not null references contests (id)
 );
 
 drop table if exists users;
 create table users (
     id integer primary key autoincrement,
-    user_name text
+    user_name text -- REFACT rename to name for consistency?
 );
 
 drop table if exists answers;
 create table answers (
     id integer primary key autoincrement,
+    -- REFACT consider to use cascading to delete answers when a user is deleted
     user_id integer not null references users (id),
     higher_ranked_descriptor_id integer not null references descriptors (id),
     lower_ranked_descriptor_id integer not null references descriptors (id)
